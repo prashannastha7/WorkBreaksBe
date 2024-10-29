9e4be490b60a6a573add33bef30baf26d8084086
prashannastha7 <prashannabdrshrestha711@gmail.com>
2024-10-29 15:33:51 +0545

    Today's Approved and Declined List

--- WebApi/Controllers/AdminController.cs
+++ WebApi/Controllers/AdminController.cs
@@ -21,6 +21,7 @@ namespace WebApi.Controllers
         public async Task<IActionResult> ApproveLeave(ApproveLeaveDto approveLeaveDto)
         {
             //FindAsync :  Retrieves an entity by its primary key.
+           
             var leave = await _context.LeaveApplications.FindAsync(approveLeaveDto.LeaveId);
 
             if (leave == null)
@@ -39,6 +40,7 @@ namespace WebApi.Controllers
         public async Task<IActionResult> DeclineLeave(ApproveLeaveDto approveLeaveDto)
         {
             //FindAsync :  Retrieves an entity by its primary key.
+           
             var leave = await _context.LeaveApplications.FindAsync(approveLeaveDto.LeaveId);
 
             if (leave == null)
@@ -79,14 +81,16 @@ namespace WebApi.Controllers
         public async Task<IActionResult> GetLeaveApplications()
         {
             //Single Database Call: By using .Include, you minimize the number of database calls.
-            //Instead of fetching leave applications and then fetching user details in a separate call,
-            //both sets of data are retrieved in one go.
+            //Instead of fetching leave applications and then fetching user details in a separate call, both sets of data are retrieved in one go.
+           
             var applications = await _context.LeaveApplications
                 .Where(u => u.Status == "Pending")
                 .Include(l => l.User)
                 .ToListAsync();
+            
             //This ensures that the data sent to the client meets the expected format.
             //Leave enum 1 2 3 ma thiyo soo string ma pathauna yoo gareyko
+            
             var applicationsDto = applications.Select(a => new ApplyLeaveDto
             {
                 StartDate = a.StartDate.ToString("yyyy/MM/dd"),
@@ -98,14 +102,73 @@ namespace WebApi.Controllers
             return Ok(applicationsDto);
         }
 
-        [HttpGet("leave/approved")]
-        public async Task<IActionResult> GetApprovedLeave()
+        //[HttpGet("leave/approved")]
+        //public async Task<IActionResult> GetApprovedLeave()
+        //{
+        //    var result = await _context.LeaveApplications
+        //       .Where(l => l.Status == "Approved")
+        //        .Include(l => l.User)
+        //        .ToListAsync();
+            
+        //    var applicationsDto = result.Select(a => new ApplyLeaveDto
+        //    {
+        //        StartDate = a.StartDate.ToString("yyyy/MM/dd"),
+        //        EndDate = a.EndDate.ToString("yyyy/MM/dd"),
+        //        LeaveType = a.LeaveType.ToString(),
+        //        Username = a.User.Username
+        //    });
+        //    return Ok(applicationsDto);
+        //}
+
+        [HttpGet("leave/approved/today")]
+        public async Task<IActionResult> GetTodayApprovedLeave()
         {
+            DateOnly today = DateOnly.FromDateTime(DateTime.Now); // Get today's date as DateOnly
+
+            var approvedLeaves = await _context.LeaveApplications
+                .Where(l => l.Status == "Approved"
+                             && l.StartDate <= today && l.EndDate >= today)
+                .Include(a => a.User)
+                .ToListAsync();
+
+
+            var applicationsDto = approvedLeaves.Select(a => new ApplyLeaveDto
+            {
+                StartDate = a.StartDate.ToString("yyyy/MM/dd"),
+                EndDate = a.EndDate.ToString("yyyy/MM/dd"),
+                LeaveType = a.LeaveType.ToString(),
+                Username = a.User.Username
+            });
+            return Ok(applicationsDto);
+        }
+
+        //[HttpGet("leave/declined")]
+        //public async Task<IActionResult> GetDeclinedLeave()
+        //{             var result = await _context.LeaveApplications
+        //       .Where(l => l.Status == "Declined")
+        //        .Include(l => l.User)
+        //        .ToListAsync();
+
+        //    var applicationsDto = result.Select(a => new ApplyLeaveDto
+        //    {
+        //        StartDate = a.StartDate.ToString("yyyy/MM/dd"),
+        //        EndDate = a.EndDate.ToString("yyyy/MM/dd"),
+        //        LeaveType = a.LeaveType.ToString(),
+        //        Username = a.User.Username
+        //    });
+        //    return Ok(applicationsDto);
+        //}
+
+        [HttpGet("leave/declined/today")]
+        public async Task<IActionResult> GetDeclinedLeave()
+        {
+            var today = DateOnly.FromDateTime(DateTime.Now); // Get today's date as DateOnly
             var result = await _context.LeaveApplications
-               .Where(l => l.Status == "Approved")
+               .Where(l => l.Status == "Declined" &&
+               l.StartDate <= today && l.EndDate >= today)
                 .Include(l => l.User)
                 .ToListAsync();
-            
+
             var applicationsDto = result.Select(a => new ApplyLeaveDto
             {
                 StartDate = a.StartDate.ToString("yyyy/MM/dd"),
@@ -116,10 +179,11 @@ namespace WebApi.Controllers
             return Ok(applicationsDto);
         }
 
-        [HttpGet("leave/declined")]
-        public async Task<IActionResult> GetDeclinedLeave()
-        {             var result = await _context.LeaveApplications
-               .Where(l => l.Status == "Declined")
+        [HttpGet("leave/pending")]
+        public async Task<IActionResult> GetPendingLeave()
+        {
+            var result = await _context.LeaveApplications
+               .Where(l => l.Status == "Pending")
                 .Include(l => l.User)
                 .ToListAsync();
             
@@ -132,19 +196,5 @@ namespace WebApi.Controllers
             });
             return Ok(applicationsDto);
         }
-        
-        //[HttpGet("leave/stats")]
-        //public async Task<IActionResult> GetLeaveStats()
-        //{
-        //    var stats = await _context.LeaveApplications
-        //        .GroupBy(l => l.LeaveType)
-        //        .Select(g => new
-        //        {
-        //            LeaveType = g.Key.ToString(),
-        //            Count = g.Count()
-        //        })
-        //        .ToListAsync();
-        //    return Ok(stats);
-        //}
     }
 }
